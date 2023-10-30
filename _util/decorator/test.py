f627681b7693c8f1b67928efa2c39a153275f88d
Gabriel Garbes <galvao.gabrielg@gmail.com>
2023-10-30 16:15:15 -0300

    gitignore

--- _util/decorator/test.py
+++ _util/decorator/test.py
@@ -4,8 +4,8 @@ import time
 @log_etl_process('etl_log.csv')
 def my_etl_process():
     # Simule sua carga de ETL aqui
-    records = ['record1', 'record2', 'record3']
-    time.sleep(2)
+    records = ['record1', 'record2', 'record3','record1', 'record2', 'record3','record1', 'record2', 'record3','record1', 'record2', 'record3']
+    time.sleep(3)
     return records
 
 if __name__ == "__main__":
